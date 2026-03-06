bf4276b6cebf6c56d853b1c7ca07bd34f20ad1f8
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-06 15:32:51 +0700

    menambahkan prosedur display

--- bubble.cpp
+++ bubble.cpp
@@ -32,3 +32,16 @@ void input()
         cin >> a[i];
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Elemen array telah terurut" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke - " << i << " : " << a[i] << endl;
+    }
+}
