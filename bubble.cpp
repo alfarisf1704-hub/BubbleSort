fe57462032258271992ba15b5b2d877d63cff9bc
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-06 15:16:32 +0700

    Implementasi while true dan if

--- bubble.cpp
+++ bubble.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+        }
+    }
+}
